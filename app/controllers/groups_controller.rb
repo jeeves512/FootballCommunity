301234210d32836f5f97d0880167fe3159ac3024
Jeevan <jeeves512@gmail.com>
2021-05-12 22:18:17 +0100

    update group controller and view

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,52 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: %i[ show edit update destroy ]
+  
+  def index
+  	@groups = Group.all
+  end
+
+  def new
+  	@group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to @group, notice: "Group was successfully created." }
+        format.json { render :show, status: :created, location: @group }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    @group.destroy
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  def show
+  end
+
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_group
+      @group = Group.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def group_params
+      params.require(:group).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   get 'group/index'
   devise_for :users
+  resources :groups#, only: [:index, :new, :show, :edit, :destroy]
   root 'home#index'
 end
